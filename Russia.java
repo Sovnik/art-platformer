d8a7598f0eb6e054237e370b348d5378188565eb
Sovnik <gravitirax@gmail.com>
2017-05-18 10:57:46 -0700

    Added Russia

---
+++ Russia.java
@@ -0,0 +1,20 @@
+// Author: Jonathan Klingspon
+
+ import java.awt.Color;
+ import java.awt.Graphics;
+ import java.awt.Graphics2D;
+ import java.awt.Rectangle;
+ import java.awt.geom.Ellipse2D;
+ import java.awt.geom.Line2D;
+ import java.awt.geom.QuadCurve2D;
+ import javax.swing.JComponent;
+
+public class Russia extends JComponent{
+
+    public void paintComponent(Graphics g){
+    
+        Graphics2D g2 = (Graphics2D) g;
+
+        Rectangle top = new Rectangle(0, 0, 200, 100)
+    }
+}
