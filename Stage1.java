6b45e5e0cf8bdb983daf3e9a430462f8261b0e5b
a <a>
2017-05-25 11:23:29 -0700

    asd
    ad

--- Stage1.java
+++ Stage1.java
@@ -6,18 +6,101 @@
  * @version (a version number or a date)
  */
 import javax.swing.JFrame;
-public class Stage1
-{
-    public static void main(String[] args)
-    {
+import java.util.Scanner;
+public class Stage1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Type a flag to print it, to exit type anything not a string");
         JFrame frame = new JFrame();
         frame.setSize(916 , 636);
         frame.setTitle("Stage");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        Greenland component = new Greenland();
-        frame.add(component);
-        
-        frame.setVisible(true);
+        while(in.hasNextLine()) {
+            String flag = in.nextLine();
+
+            if (flag.equalsIgnoreCase("France")) {
+                France component = new France();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("Japan")) {
+                Japan component = new Japan();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("Ukraine")) {
+                Ukraine component = new Ukraine();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("Austria")) {
+                Austria component = new Austria();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("Thailand")) {
+                Thailand component = new Thailand();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("China")) {
+                China component = new China();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("Hungary")) {
+                Hungary component = new Hungary();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("Bangladesh")) {
+                Bangladesh component = new Bangladesh();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("Belgium")) {
+                Belgium component = new Belgium();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("Greenland")) {
+                Greenland component = new Greenland();
+                frame.add(component);
+            }
+            if (flag.equalsIgnoreCase("close")) {
+                break;
+            }
+            frame.setVisible(true);
+            
+            System.out.println("Type a flag to print it, to exit type anything not a string");
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
